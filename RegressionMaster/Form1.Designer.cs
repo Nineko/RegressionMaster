c7ba0ff282f4d980b8fc6ea9a5692bffcfc24d0c
Nineko <magacato1532@gmail.com>
2020-12-06 19:32:02 +0800

    加入專案檔案。

---
+++ RegressionMaster/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace RegressionMaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DebubText = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(343, 412);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView1_KeyPress);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView2.Location = new System.Drawing.Point(371, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(343, 412);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.dataGridView2_KeyPress);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Column1";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Column2";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Column3";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // DebubText
+            // 
+            this.DebubText.BackColor = System.Drawing.SystemColors.InfoText;
+            this.DebubText.ForeColor = System.Drawing.SystemColors.Window;
+            this.DebubText.Location = new System.Drawing.Point(721, 13);
+            this.DebubText.Name = "DebubText";
+            this.DebubText.Size = new System.Drawing.Size(151, 409);
+            this.DebubText.TabIndex = 2;
+            this.DebubText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 434);
+            this.Controls.Add(this.DebubText);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.RichTextBox DebubText;
+    }
+}
+
---
+++ RegressionMaster/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegressionMaster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        public void PasteInData(ref DataGridView dgv)
+        {
+            this.DebubText.AppendText("PasteInData");
+            char[] rowSplitter = { '\n', '\r' };  // Cr and Lf.
+            char[] columnSplitter = { '\t', ',' };           // Tab. , 
+
+            IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+            string stringInClipboard =
+                dataInClipboard.GetData(DataFormats.Text).ToString();
+
+            string[] rowsInClipboard = stringInClipboard.Split(rowSplitter,
+                StringSplitOptions.RemoveEmptyEntries);
+
+            int r = dgv.SelectedCells[0].RowIndex;
+            int c = dgv.SelectedCells[0].ColumnIndex;
+
+            if (dgv.Rows.Count < (r + rowsInClipboard.Length))
+                dgv.Rows.Add(r + rowsInClipboard.Length - dgv.Rows.Count);
+
+            // Loop through lines:
+
+            int iRow = 0;
+            while (iRow < rowsInClipboard.Length)
+            {
+                // Split up rows to get individual cells:
+
+                string[] valuesInRow =
+                    rowsInClipboard[iRow].Split(columnSplitter);
+
+                // Cycle through cells.
+                // Assign cell value only if within columns of grid:
+
+                int jCol = 0;
+                while (jCol < valuesInRow.Length)
+                {
+                    if ((dgv.ColumnCount - 1) >= (c + jCol))
+                        dgv.Rows[r + iRow].Cells[c + jCol].Value =
+                        valuesInRow[jCol];
+
+                    jCol += 1;
+                } 
+
+                iRow += 1;
+            } 
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.DebubText.AppendText("Form1_KeyPress");
+            char value = e.KeyChar;
+            int key = (int)value;
+            if (key != 22)
+                return;
+            PasteInData(ref this.dataGridView1);
+        }
+
+        private void dataGridView2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.DebubText.AppendText("Form2_KeyPress");
+            char value = e.KeyChar;
+            int key = (int)value;
+            if (key != 22)
+                return;
+            PasteInData(ref this.dataGridView2);
+        }
+    }
+
+}
